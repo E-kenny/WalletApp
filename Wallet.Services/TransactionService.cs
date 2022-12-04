e065af3cf4b7ba6da99cdf619055629a855ab104
cvbanns <oluwaiteadewole@gmail.com>
2022-12-04 14:01:51 +0100

    Add-bg-getRate and currency converter

--- Wallet.Services/TransactionService.cs
+++ Wallet.Services/TransactionService.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Transactions;
 using WalletApp.Abstractions.Repositories;
@@ -13,14 +15,63 @@ namespace WalletApp.Services
     public class TransactionService : ITransactionService
     {
         private readonly ITransactionRepository _transactionRepository;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public TransactionService(ITransactionRepository transactionRepository)
+        public TransactionService(ITransactionRepository transactionRepository, IHttpClientFactory httpClientFactory)
         {
             _transactionRepository = transactionRepository;
+            _httpClientFactory = httpClientFactory;
         }
-        public Task<double?> ConvertCurrency(string currencyToConvert, double amount)
+        
+        public async Task<double?> ConvertCurrency(string currencyA, string currencyB, double amount)
         {
-            throw new NotImplementedException();
+                try
+                {
+                    string conversion_result = string.Empty;
+                    var httpClient = _httpClientFactory.CreateClient();
+                    var conversionStr = $"https://v6.exchangerate-api.com/v6/033ce5eb6f40fe61f7080372/pair/{currencyA}/{currencyB}/{amount}";
+                    using (var response = await httpClient.GetAsync(conversionStr, HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var stream = await response.Content.ReadAsStreamAsync();
+                        var newConvertDouble = await JsonSerializer.DeserializeAsync<ConvertDoubleDTO>(stream);
+                        var newAmount = newConvertDouble.conversion_result;
+
+
+                        return newAmount;
+                    }
+
+                }
+                catch (Exception)
+                {
+                    return default;
+
+                }
+
+        }
+        public async Task<double?> GetRate(string currencyCode, double amount)
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var conversionStr = "https://open.er-api.com/v6/latest/NGN";
+                using (var response = await httpClient.GetAsync(conversionStr, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    var newRates = await JsonSerializer.DeserializeAsync<RatesDTO>(stream);
+                    var rate = newRates.rates[currencyCode];
+                    var convAmount = rate * amount;
+
+                    return convAmount;
+                }
+
+            }
+            catch (Exception)
+            {
+                return default;
+
+            }
         }
 
         public Task<IEnumerable<TransactionDTO>> GetAllUserTransactions(int UserId)
@@ -28,9 +79,11 @@ namespace WalletApp.Services
             throw new NotImplementedException();
         }
 
+        
         public Task<IEnumerable<TransactionDTO>> GetWalletStatement(int WalletId)
         {
             throw new NotImplementedException();
         }
+    
     }
 }
