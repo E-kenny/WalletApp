6b4b1f0399d81c38f23a3e548d04d09450c69f1c
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2022-12-06 00:10:30 +0100

    walletApp-bg-authorize attribute

--- WalletApp/Controllers/WalletController.cs
+++ WalletApp/Controllers/WalletController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using WalletApp.Abstractions.Services;
@@ -6,6 +7,7 @@ using WalletApp.Models.DTO;
 
 namespace WalletApp.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class WalletController : ControllerBase
